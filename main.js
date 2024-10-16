c2c8374b5e6fab72148ebbf895f54fe2400af588
taufikraden29 <taufikraden29@gmail.com>
2024-10-16 19:30:28 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,57 @@
+// function addName(firstname, lastname) {
+//     return firstname;
+//   console.log(firstname, lastname);
+// }
+
+// addName("Raden". "Taufik");
+
+// const fullname = function addName(firstname, lastname) {
+//   // return firstname;
+//   console.log(firstname, lastname);
+// };
+
+// fullname("Raden", "Taufik");
+
+// function addName(firstname, lastname) {
+//   return firstname;
+//   console.log(firstname, lastname);
+// }
+
+// // Cara 1
+// const sum1 = new Function("x", "y", "return x+y");
+
+// // Cara 2
+// function sum2(x, y) {
+//   console.log(x + y);
+// }
+
+// // Cara 3
+// const sum3 = (x, y) => {
+//   console.log(x + y);
+// };
+// sum3(5, 5);
+
+// function lingkaran() {
+//   const r = parseInt(prompt("Berapa jari-jarinya ? "));
+//   const luas = 3.14 * r * r;
+//   alert("Luasnya adalah " + luas + " cm");
+// }
+// lingkaran();
+
+// Soal 2
+// let nama = prompt("Nama karyawan :");
+// let gaji = parseInt(prompt("Gaji perhari :"));
+// let hari = parseInt(prompt("Jumlah hari masuk kerja :"));
+// totalGaji(gaji, hari);
+// function totalGaji(gaji, hari) {
+//   const total = gaji * hari;
+//   alert(
+//     "Karyawan bernama " +
+//       nama +
+//       " bekerja selama " +
+//       hari +
+//       " hari memiliki gaji sebulan sebesar Rp " +
+//       total
+//   );
+// }
+// totalGaji();
